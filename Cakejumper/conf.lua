5cefe3e2d5739e517572c57e4825408cabff4238
dumbie <dumb.dumber>
2014-11-04 16:13:54 +0100

    hdfhadfjhañkdshfñashdfa

--- Cakejumper/conf.lua
+++ Cakejumper/conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	t.title ="CakeJumper"
 	t.screen.width = 384
-	t.screen.height = 621
+	t.screen.height = 622
 	screenWidth = t.screen.width
 	screenHeight = t.screen.height 
 end
